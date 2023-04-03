d4801b5a45597ddfdf547d2ef71303e58bb3954b
dhm98 <dhm98@>
2023-04-03 21:29:29 +0900

    초기 커밋.

---
+++ Project4-1/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_SIZE 10
+
+int main(void) {
+	int i;
+	srand(time(NULL));
+		printf("-----Using array-----\n");
+		for (i = 1; i <= 30; i++){
+			if (i % 2 == 0)
+				printf("[%d] : push %d\n", i,rand() % 100 + 1);
+			else if (i % 2 == 1)
+				printf("[%d] : pop %d\n",i ,rand() % 100 + 1);
+
+	}
+	return 0;
+}
